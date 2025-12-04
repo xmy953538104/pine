efecbde4d5e43124ab00c752cf3c56114c3098a9
MY <xmy953538104@gmail.com>
2025-12-04 18:17:41 +0800

    Update settings.h

--- app/src/main/cpp/skyline/common/settings.h
+++ app/src/main/cpp/skyline/common/settings.h
@@ -72,7 +72,7 @@ namespace skyline {
 
         // Display
         Setting<bool> forceTripleBuffering; //!< If the presentation engine should always triple buffer even if the swapchain supports double buffering
-        Setting<int> vsyncMode;
+        Setting<bool> disableFrameThrottling; //!< Allow the guest to submit frames without any blocking calls
         Setting<bool> useAsyncShaders;
         Setting<bool> disableShaderCache;  //!< Prevents cached shaders from being loaded and disables caching of new shaders
 
